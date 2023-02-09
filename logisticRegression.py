1855247f08d03942c2f1feb8e9fb1c97c796217a
pictorviktor <viktor.e.evestam@gmail.com>
2023-02-09 16:30:13 +0100

    new models

---
+++ logisticRegression.py
@@ -0,0 +1,42 @@
+import numpy as np
+import pandas as pd
+import sklearn.linear_model as skl_lm
+import sklearn.model_selection as skl_ms
+from pandas.io.parsers.readers import read_csv
+
+#Importing
+train = pd.read_csv('/Users/admin/Documents/CodeProjects/numpy_projects/train.csv').dropna().reset_index(drop=True)
+#train.info()
+
+# Choose output variable
+X = train.drop(columns=['Lead'])
+y = train['Lead']
+
+# Split parameters
+n_fold = 10
+cv = skl_ms.KFold(n_splits = n_fold, random_state = 1, shuffle = True)
+
+# Visualizing
+from IPython.display import set_matplotlib_formats
+set_matplotlib_formats('svg') # Output as svg. Else you can try png
+from IPython.core.pylabtools import figsize
+figsize(10, 6) # Width and hight
+np.set_printoptions(precision=3)
+
+# Model
+np.random.seed(1)
+model = skl_lm.LogisticRegression(solver='liblinear')
+X_train, X_test, y_train, y_test = skl_ms.train_test_split(X,y,test_size=0.3)
+
+model.fit(X_train, y_train)
+print(model)
+predict_prob=model.predict_proba(X_test)
+print(model.classes_)
+predict_prob[0:5]
+prediction=np.empty(len(X_test), dtype=object)
+prediction=np.where(predict_prob[:, 0]>=0.5, 'Female', 'Male')
+prediction[0:5]
+
+#Confusion matrix
+print(pd.crosstab(prediction, y_test), '\n')
+print(f"Accuracy: {np.mean(prediction == y_test):.3f}")
--- randomForest.py
+++ randomForest.py
@@ -30,42 +30,34 @@ trainingSet = train.iloc[trainingIndex]
 testSet = train.iloc[~train.index.isin(trainingIndex)]   
 # print(train.info())
 # print(train.head())
-x_train = trainingSet.copy().drop(columns=['Lead','Number of male actors', 'Number of female actors', 
-'Mean Age Male', 'Mean Age Female'])
+x_train = trainingSet.copy().drop(columns=['Lead'])
 y_train = trainingSet['Lead']
-x_test = testSet.copy().drop(columns=['Lead','Number of male actors', 'Number of female actors', 
-'Mean Age Male', 'Mean Age Female'])
+x_test = testSet.copy().drop(columns=['Lead'])
 y_test = testSet['Lead']
 
 
 # Modeling
-
-# model = tree.DecisionTreeClassifier(max_leaf_nodes=5)
-# model.fit(X=x_train, y=y_train)
-# y_predict = model.predict(x_test)
-
-model = RandomForestClassifier(n_estimators=100)
+model = RandomForestClassifier(n_estimators=200) #Adding oob_score, max_depth or more estimators- 
+#does not result in better model in this case
 model.fit(x_train, y_train)
 y_pred = model.predict(x_test)
 accuracy = accuracy_score(y_test, y_pred)
 error = round(np.mean(model.predict(x_test) != y_test),3)
-print('Test error:', error)
-print('accuracy', accuracy)
-print(accuracy+error)
+print('Test error:', error,' and accuracy', accuracy)
 
 # Perform cross-validation
 scores = cross_val_score(model, x_train, y_train, cv=10)
 
 # Calculate the mean accuracy across the 5 folds
 mean_accuracy = np.mean(scores)
-print(scores)
+#print(scores)
 print('mean accuracy', mean_accuracy)
 #Naive guess
 
 naive_guess = train[train['Lead'] == 'Male']
 number_of_data_points = naive_guess.shape[0]
-print('Naive guess is', number_of_data_points, 'out of', train.shape[0], 'is male')
-print('this equals to', round(number_of_data_points/train.shape[0], 3), 'of the whole dataset')
+# print('Naive guess is', number_of_data_points, 'out of', train.shape[0], 'is male')
+# print('this equals to', round(number_of_data_points/train.shape[0], 3), 'of the whole dataset')
 
 # #Visualizing the graph
 # dot_data = export_graphviz(
